461d7935f0b81cf3fe286720c053acdbd97c425a
Matheus <menesesmatheus993@gmail.com>
2024-11-16 15:51:20 -0300

    Realizado a construção do método atualizar informações de pacientes e excluir pacientes

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -2,11 +2,13 @@ package med.voll.api.paciente;
 
 import jakarta.persistence.*;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import med.voll.api.endereco.Endereco;
+import med.voll.api.medico.DadosAtualizaçaoMedico;
 
 @Getter
 @EqualsAndHashCode(of = "id")
@@ -27,7 +29,11 @@ public class Paciente {
     @Embedded
     private Endereco endereco;
 
+    private Boolean ativo;
+
+
     public Paciente(DadosCadastroPaciente dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -35,4 +41,20 @@ public class Paciente {
         this.endereco = new Endereco(dados.endereco());
     }
 
+    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+        if (dados.nome() != null){
+            this.nome = dados.nome();
+        }
+        if (dados.telefone() != null){
+            this.nome = dados.telefone();
+        }
+        if (dados.endereco() != null){
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+
+    }
+
+    public void inativar() {
+        this.ativo = false;
+    }
 }
